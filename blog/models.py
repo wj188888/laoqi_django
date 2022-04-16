400f9987d23313e122fc3e9c7e0f8ab367a4d321
wangjie <2460665525@qq.com>
2022-04-16 21:11:34 +0800

    又近了一小步

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 class BlogArticles(models.Model):
     title = models.CharField(max_length=300)
