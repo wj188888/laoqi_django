fc7431747318c4bfaf5c5e9ac54e8ddaf54a2a58
wangjie <2460665525@qq.com>
2022-04-19 19:54:40 +0800

    新增加+3

--- account/models.py
+++ account/models.py
@@ -2,11 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
+    """用户注册编辑"""
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     birth = models.DateField(blank=True, null=True)
     phone = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return 'user {}'.format(self.user.username)
+
+class UserInfo(models.Model):
+    """用心详情信息"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    school = models.CharField(max_length=100, blank=True)
+    company = models.CharField(max_length=100, blank=True)
+    profession = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    aboutme = models.TextField(blank=True)
+
+    def __str__(self):
+        return "user:{}".format(self.user.username)
 
 
